8b4994ff52bfe266f8c72f717110d7ed07df12d8
nasrullah-nobin <nasrullahbd82@gmail.com>
2026-02-22 18:54:55 +0600

    correct render logic and ensure interview moves from reject list

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ let currentFilter = "all";
 let totalCount = document.getElementById("total-count");
 let interviewCount = document.getElementById("interview-count");
 let rejectCount = document.getElementById("reject-count");
+let availableJobsCount = document.getElementById("Available-jobs");
 
 const allFilterBtn = document.getElementById("all-filter-btn");
 const interviewFilterBtn = document.getElementById("interview-filter-btn");
@@ -19,6 +20,7 @@ function calculateCount() {
   totalCount.innerText = getTotalCount();
   interviewCount.innerText = interviewList.length;
   rejectCount.innerText = rejectList.length;
+  availableJobsCount.innerText = getTotalCount();
 }
 calculateCount();
 
@@ -37,31 +39,29 @@ function filteredBtn(currentId) {
   if (currentId === "all-filter-btn") {
     allCardContainer.classList.remove("hidden");
     noJobsSection.classList.add("hidden");
-    filterSection.classList.add("hidden");
   } else if (currentId === "interview-filter-btn") {
     allCardContainer.classList.add("hidden");
     noJobsSection.classList.remove("hidden");
-    filterSection.classList.remove("hidden");
-
     renderInterview();
   } else if (currentId === "reject-filter-btn") {
     allCardContainer.classList.add("hidden");
     noJobsSection.classList.remove("hidden");
+    renderReject();
   }
 }
 
 mainContainer.addEventListener("click", function (event) {
-  const card = event.target.closest("#all-card-container");
-  if (!card) return;
-  const companyName = card.querySelector(".company-name").innerText;
-  const jobPosition = card.querySelector(".job-position").innerText;
-  const jobLocation = card.querySelector(".job-location").innerText;
-  const jobStatus = card.querySelector(".job-status");
-  const jobDescription = card.querySelector(".job-description").innerText;
-
   if (event.target.classList.contains("interview-btn")) {
-    jobStatus.innerText = "Interview";
-    jobStatus.className = "btn btn-outline btn-success";
+    const parentNode = event.target.parentNode.parentNode;
+
+    const companyName = parentNode.querySelector(".company-name").innerText;
+    const jobPosition = parentNode.querySelector(".job-position").innerText;
+    const jobLocation = parentNode.querySelector(".job-location").innerText;
+    const jobStatus = parentNode.querySelector(".job-status").innerText;
+    const jobDescription =
+      parentNode.querySelector(".job-description").innerText;
+    parentNode.querySelector(".job-status").innerText = "INTERVIEW";
+    // jobStatus.className = "btn btn-outline btn-success";
     const cardInfo = {
       companyName,
       jobPosition,
@@ -72,13 +72,52 @@ mainContainer.addEventListener("click", function (event) {
     const exists = interviewList.find(
       (item) => item.companyName === companyName,
     );
-    if (!exists) interviewList.push(cardInfo);
-    rejectList.filter((item) => item.companyName !== companyName);
+    if (!exists) {
+      interviewList.push(cardInfo);
+    }
+
+    rejectList = rejectList.filter(
+      (item) => item.companyName !== cardInfo.companyName,
+    );
+
+    if (currentFilter === "reject-filter-btn") {
+      renderReject();;
+    }
+
+    calculateCount();
+  } else if (event.target.classList.contains("reject-btn")) {
+    const parentNode = event.target.parentNode.parentNode;
+
+    const companyName = parentNode.querySelector(".company-name").innerText;
+    const jobPosition = parentNode.querySelector(".job-position").innerText;
+    const jobLocation = parentNode.querySelector(".job-location").innerText;
+    const jobStatus = parentNode.querySelector(".job-status").innerText;
+    const jobDescription =
+      parentNode.querySelector(".job-description").innerText;
+    parentNode.querySelector(".job-status").innerText = "REJECTED";
+    // jobStatus.className = "btn  btn-outline btn-error";
+    const cardInfo = {
+      companyName,
+      jobPosition,
+      jobLocation,
+      jobStatus,
+      jobDescription,
+    };
+    const exists = rejectList.find(
+      (item) => item.companyName === cardInfo.companyName,
+    );
+    if (!exists) rejectList.push(cardInfo);
+
+    interviewList = interviewList.filter(
+      (item) => item.companyName !== cardInfo.companyName,
+    );
+
     if (currentFilter === "interview-filter-btn") {
       renderInterview();
     }
+
+    calculateCount();
   }
-  calculateCount();
 });
 
 function renderInterview() {
@@ -93,13 +132,43 @@ function renderInterview() {
             <p class="job-location text-[14px] text-gray-500">
              ${interview.jobLocation}
             </p>
-            <button class="job-status btn btn-outline btn-success">Interview</button>
+            <button class="job-status btn btn-outline btn-success">INTERVIEW</button>
             <p class="job-description text-gray-400">
               ${interview.jobDescription}
             </p>
             <div class="flex gap-5 justify-center md:justify-start">
-              <button class="interview-btn btn btn-outline btn-success">interview</button>
-              <button class="reject-btn btn btn-outline btn-error">Rejected</button>
+              <button class="interview-btn btn btn-outline btn-success">INTERVIEW</button>
+              <button class="reject-btn btn btn-outline btn-error">REJECTED</button>
+            </div>
+          </div>
+          <div
+            class="rounded-full h-fit p-1 border border-gray-300 hidden sm:block"
+          >
+            <i class="fa-regular fa-trash-can"></i>
+          </div>`;
+    filterSection.appendChild(newDiv);
+  });
+}
+
+function renderReject() {
+  filterSection.innerHTML = "";
+  rejectList.forEach((reject) => {
+    const newDiv = document.createElement("div");
+    newDiv.className = "bg-white p-6 rounded-lg flex justify-between";
+    newDiv.innerHTML = `
+        <div class="space-y-3 text-center md:text-left">
+            <h2 class="company-name text-lg font-semibold">${reject.companyName}</h2>
+            <p  class="job-position text-base text-gray-500">${reject.jobPosition}</p>
+            <p class="job-location text-[14px] text-gray-500">
+             ${reject.jobLocation}
+            </p>
+            <button class="job-status btn btn-outline btn-error">REJECTED</button>
+            <p class="job-description text-gray-400">
+              ${reject.jobDescription}
+            </p>
+            <div class="flex gap-5 justify-center md:justify-start">
+              <button class="interview-btn btn btn-outline btn-success">INTERVIEW</button>
+              <button class="reject-btn btn btn-outline btn-error">REJECTED</button>
             </div>
           </div>
           <div

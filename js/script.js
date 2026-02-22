f33738ff138fe2f0be8803b17a86c21337a40bd6
nasrullah-nobin <nasrullahbd82@gmail.com>
2026-02-22 06:59:11 +0600

    implement button toggle with active state styling

--- js/script.js
+++ js/script.js
@@ -1,12 +1,37 @@
+let interviewList = [];
+let rejectList= [];
+let currentFilter = 'all';
+
+
+
 let totalCount = document.getElementById('total-count');
 let interviewCount = document.getElementById('interview-count');
 let rejectCount= document.getElementById('reject-count');
 
 
+const allFilterBtn = document.getElementById('all-filter-btn');
+const interviewFilterBtn = document.getElementById('interview-filter-btn');
+const rejectFilterBtn = document.getElementById('reject-filter-btn');
+
 const mainContainer = document.querySelector('main');
 const allCardContainer = document.getElementById('all-card-container');
+const filterSection = document.getElementById('filter-section');
 
 function calculateCount(){
 totalCount.innerText= allCardContainer.childElementCount;
 }
 calculateCount()
+
+
+
+function filteredBtn(currentId){
+currentFilter = currentId;
+
+
+allFilterBtn.classList.remove('btn-info');
+interviewFilterBtn.classList.remove('btn-info');
+rejectFilterBtn.classList.remove('btn-info');
+document.getElementById(currentId).classList.add('btn-info');
+
+
+}
